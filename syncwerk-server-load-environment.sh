c7fe967f29843e1e443ed47f9d26c19ef045e164
Alexander Jackson <alexanderjackson@users.noreply.github.com>
2018-02-01 14:30:14 +0100

    Create syncwerk-server-load-environment.sh

---
+++ syncwerk-server-load-environment.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+export CCNET_CONF_DIR=/opt/syncwerk/ccnet
+CCNET_CONF_DIR=/opt/syncwerk/ccnet
+export SEAFILE_CONF_DIR=/opt/syncwerk/syncwerk-data
+SEAFILE_CONF_DIR=/opt/syncwerk/syncwerk-data
+export SEAFILE_CENTRAL_CONF_DIR=/opt/syncwerk/conf
+SEAFILE_CENTRAL_CONF_DIR=/opt/syncwerk/conf
+export PYTHONPATH=/opt/syncwerk/syncwerk-server-latest/syncwerk/lib/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib64/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/seahub:/opt/syncwerk/syncwerk-server-latest/seahub/thirdpart:
+PYTHONPATH=/opt/syncwerk/syncwerk-server-latest/syncwerk/lib/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib64/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/seahub:/opt/syncwerk/syncwerk-server-latest/seahub/thirdpart:
+export PYTHONPATH=/opt/syncwerk/syncwerk-server-latest/syncwerk/lib/python2.7/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib64/python2.7/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib64/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/seahub:/opt/syncwerk/syncwerk-server-latest/seahub/thirdpart:
+PYTHONPATH=/opt/syncwerk/syncwerk-server-latest/syncwerk/lib/python2.7/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib64/python2.7/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/syncwerk/lib64/python2.6/site-packages:/opt/syncwerk/syncwerk-server-latest/seahub:/opt/syncwerk/syncwerk-server-latest/seahub/thirdpart:
